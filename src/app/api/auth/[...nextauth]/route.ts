1099be091a5075d957edecd72d5e0b3e047b5a0e
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-05 19:52:52 +0530

    RO

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
-
 import { authOptions } from './option';
 
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  return await NextAuth(req, res, authOptions);
+};
 
 export { handler as GET, handler as POST };
