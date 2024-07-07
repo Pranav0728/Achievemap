3e8b8a78a1207130e5909d72f8594207af4e4d95
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-07 10:16:32 +0530

    env added

--- src/app/api/auth/[...nextauth]/option.ts
+++
@@ -1,7 +0,0 @@
-import { authOptions } from "./route";
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import NextAuth from "next-auth/next";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
-export { handler as GET, handler as POST };
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -45,3 +45,5 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
+export { handler as GET, handler as POST };
