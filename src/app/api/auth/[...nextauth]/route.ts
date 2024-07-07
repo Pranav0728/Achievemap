f793f7b1f8f26228f810b4d7341745e1fe11ec60
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-07 10:36:54 +0530

    res, req delete

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import { authOptions } from "./option";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
+const handler  = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
