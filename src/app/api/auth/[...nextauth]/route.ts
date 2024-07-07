c4fa6c8d42dda9d5e23d4069e80ef8e774cb9467
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-07 10:32:04 +0530

    option

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../../../lib/dbConnect';
-import User from '../../../../lib/models/users';
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import GithubProvider from 'next-auth/providers/github';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    })
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/en_lp",
-  },
-  callbacks: {
-    async jwt({ token, user, account, profile }) {
-      if ((account?.provider === 'google' || account?.provider === 'github') && profile) {
-        try {
-          await dbConnect();
-          const existingUser = await User.findOne({ email: user.email });
-          if (!existingUser) {
-            const newUser = new User({
-              email: user.email,
-              name: user.name,
-              image: user.image,
-            });
-            await newUser.save();
-          } 
-        } catch (error) {
-          console.log(error);
-        }
-      }
-      return token;
-    },
-  },
-};
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import { authOptions } from "./option";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
 
