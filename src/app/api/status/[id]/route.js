d7cc6c8541a5040134a0ab24fb684e0a981fbb51
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-12 19:42:19 +0530

    cors added

--- src/app/api/status/[id]/route.js
+++ src/app/api/status/[id]/route.js
@@ -2,24 +2,19 @@ import { NextResponse } from "next/server";
 import sha256 from "crypto-js/sha256";
 import axios from "axios";
 
-export async function POST(req, formData) {
+export async function POST(req) {
   const data = await req.formData();
   console.log(data);
+
   const status = data.get("code");
   const merchantId = data.get("merchantId");
   const transactionId = data.get("transactionId");
 
-  const st =
-    `/pg/v1/status/${merchantId}/${transactionId}` +
-    process.env.NEXT_PUBLIC_SALT_KEY;
-  // console.log(st)
-  const dataSha256 = sha256(st);
+  const st = `/pg/v1/status/${merchantId}/${transactionId}${process.env.NEXT_PUBLIC_SALT_KEY}`;
+  const dataSha256 = sha256(st).toString(); // Convert to string
 
-  const checksum = dataSha256 + "###" + process.env.NEXT_PUBLIC_SALT_INDEX;
+  const checksum = `${dataSha256}###${process.env.NEXT_PUBLIC_SALT_INDEX}`;
   console.log(checksum);
-
-
-
 
   const options = {
     method: "GET",
@@ -28,23 +23,28 @@ export async function POST(req, formData) {
       accept: "application/json",
       "Content-Type": "application/json",
       "X-VERIFY": checksum,
-      "X-MERCHANT-ID": `${merchantId}`,
+      "X-MERCHANT-ID": merchantId,
     },
   };
 
-  // CHECK PAYMENT STATUS
-  const response = await axios.request(options);
-  console.log("r===", response.data.code);
+  try {
+    // CHECK PAYMENT STATUS
+    const response = await axios.request(options);
+    console.log("r===", response.data.code);
 
-
-  if (response.data.code == "PAYMENT_SUCCESS")
-  return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/success`,{
-    status: 301,
-  });
-else return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/failure`,{
-  // a 301 status is required to redirect from a POST to a GET route
-  status: 301,
-});
-
-
+    if (response.data.code === "PAYMENT_SUCCESS") {
+      return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/success`, {
+        status: 301,
+      });
+    } else {
+      return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/failure`, {
+        status: 301,
+      });
+    }
+  } catch (error) {
+    console.error("Error in API request:", error.response.data);
+    return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/failure`, {
+      status: 301,
+    });
+  }
 }
