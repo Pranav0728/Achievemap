e4b1640fa690acac90a1879e6ca4f77bbd2c80b9
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-14 00:54:47 +0530

    purchased done

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,4 @@
 
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ['/checkout', '/roadmaps/:path*']}
+export const config = { matcher: ['/checkout', '/roadmaps/:path*', '/services/:']}
