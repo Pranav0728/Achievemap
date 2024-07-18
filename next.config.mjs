bb76c2b130ebf4f708a1b3a68a148bc3e34c190f
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-18 20:22:39 +0530

    Proper URL push

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com'],
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              connect-src 'self';
+              font-src 'self';
+              frame-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              form-action 'self';
+            `.replace(/\n/g, ''), // Removing newlines for a clean header value
+          },
+        ],
       },
+    ];
+  },
 };
 
 export default nextConfig;
