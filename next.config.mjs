2d234d5d6d909d17eb4a09d8b1ab04a9dad88c55
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-07-18 20:49:23 +0530

    Proper URL push

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data:;
-              connect-src 'self';
-              font-src 'self';
-              frame-src 'self';
-              object-src 'none';
-              base-uri 'self';
-              form-action 'self';
-            `.replace(/\n/g, ''), // Removing newlines for a clean header value
-          },
-        ],
+    images: {
+        domains: ['lh3.googleusercontent.com'],
       },
-    ];
-  },
 };
 
 export default nextConfig;
--- src/app/actions/serveractions.js
+++ src/app/actions/serveractions.js
@@ -13,7 +13,7 @@ export async function payment(amount, uid, id) {
     amount: amount,
     redirectUrl: `${process.env.NEXTAUTH_URL}/api/status/${transactionId}?uid=${uid}&id=${id}`,
     redirectMode: "POST",
-    callbackUrl: `${process.env.NEXTAUTH_URL}/api/status/${transactionId}?uid=${uid}&id=${uid}`,
+    callbackUrl: `${process.env.NEXTAUTH_URL}/api/status/${transactionId}?uid=${uid}&id=${id}`,
     mobileNumber: "9999999999",
     paymentInstrument: {
       type: "PAY_PAGE",
@@ -27,12 +27,13 @@ export async function payment(amount, uid, id) {
   console.log("Base64 Payload:", dataBase64);
 
   const fullURL = dataBase64 + "/pg/v1/pay" + process.env.NEXT_PUBLIC_SALT_KEY;
-  const dataSha256 = sha256(fullURL);
+  const dataSha256 = sha256(fullURL).toString();
   const checksum = dataSha256 + "###" + process.env.NEXT_PUBLIC_SALT_INDEX;
   console.log("Checksum:", checksum);
 
   const UAT_PAY_API_URL = `${process.env.NEXT_PUBLIC_UAT_ID}/pg/v1/pay`;
-  console.log(UAT_PAY_API_URL)
+  console.log("UAT_PAY_API_URL:", UAT_PAY_API_URL);
+
   try {
     const response = await fetch(UAT_PAY_API_URL, {
       method: "POST",
@@ -41,7 +42,7 @@ export async function payment(amount, uid, id) {
         "Content-Type": "application/json",
         "X-VERIFY": checksum,
       },
-      body: JSON.stringify({ request: dataBase64 }),
+      body: JSON.stringify({ request: dataBase64, uid, id }), // Include uid and id in the POST body
     });
 
     if (!response.ok) {
@@ -53,7 +54,7 @@ export async function payment(amount, uid, id) {
     const redirect = responseData.data.instrumentResponse.redirectInfo.url;
 
     // Store initial transaction details in the database
-    return redirect
+    return redirect;
   } catch (error) {
     console.error("Error:", error.message);
     throw error;
